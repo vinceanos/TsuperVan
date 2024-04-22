67c5323b512ad90d7f60d90549149dc336254e12
anyossystem <france_visarra@yahoo.com>
2024-04-22 14:43:39 +0800

    a
    a

---
+++ Controllers/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tsupervan
+{
+    public class Login
+    {
+        public int Email { get; set; }
+        public string Password { get; set; }
+    }
+}
